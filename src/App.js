bf39308e107a3495e22f9bc886c85bd2813a3fc3
NusairTasin <tasinnusair@gmail.com>
2023-12-16 01:30:56 +0600

    Header Complete

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { Footer, Blog, Possibility, Feature, WhatGPT3, Header, Features } from './containers';
+import { CTA, Brand, Navbar } from './components'
+
+import './App.css';
+
+const App = () => {
+    return (
+        <div className='App'>
+            <div className='gradient__bg'>
+                <Navbar />
+                <Header />
+            </div>
+            <div>
+                <Brand />
+                <WhatGPT3 />
+                <Features />
+                <Possibility />
+                <CTA />
+                <Blog />
+                <Footer />
+            </div>
+        </div>
+    )
+}
+
+export default App
